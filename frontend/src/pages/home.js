c3e5bae9da91fd77018dd4715c6b29261292387d
Ian Tjok <ianptjok@hotmail.com>
2025-03-12 23:25:41 -0700

    Updated home.js to use search bar
    - Centered title
    - Added search bar
    - Changed VideoList to pass in filteredVideos as a prop (obtained by using the useVideos hook)

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
-import VideoUpload from "../components/videoUpload";
-import VideoPlayer from "../components/videoPlayer";
 import VideoList from "../components/videoList";
+import SearchBar from "../components/searchbar";
+import useVideos from "../hooks/useVideos"; 
 
 const Home = () => {
+    // using hook
+    const { filteredVideos, handleSearch, loading, error } = useVideos();
+
     return (
         <div>
             <Navbar />
-            <h1 className="p-4">Welcome to YouTube Clone</h1>
-            <VideoList /> {/* Show videos on homepage */}
+            <h1 style={{ textAlign: "center"}}> Welcome to YouTube Clone</h1>
+            <SearchBar onSearch={handleSearch} />
+            {loading && <p>Loading videos...</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <VideoList videos={filteredVideos} />
         </div>
     );
 };
+
 export default Home;
