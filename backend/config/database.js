1f2281abd92fa4af05faa914067eb7339e09c705
joshishruti9 <joshishruti009@gmail.com>
2025-03-13 12:35:35 -0700

    Updated database.js to implement singleton pattern

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,33 @@
-// models/videoModel.js
 const mysql = require('mysql2');
 
-// MySQL database connection
-const db = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "ApplePie", // Change this to your MySQL password
-    database: "youtube_app"
-});
+class Database {
+    constructor() {
+        if (!Database.instance) {
+            this.connection = mysql.createConnection({
+                host: "127.0.0.1",
+                user: "root",
+                password: "root", // Change this to your MySQL password
+                database: "youtube_app"
+            });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database: ', err);
-        return;
+            this.connection.connect((err) => {
+                if (err) {
+                    console.error('Error connecting to the database: ', err);
+                    return;
+                }
+                console.log('Connected to MySQL!');
+            });
+
+            Database.instance = this;
+        }
+        return Database.instance;
     }
-    console.log('Connected to MySQL!');
-});
 
-module.exports = db; 
+    getConnection() {
+        return this.connection;
+    }
+}
+
+// Export the singleton instance
+const dbInstance = new Database();
+module.exports = dbInstance.getConnection();
