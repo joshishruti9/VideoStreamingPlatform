54003b9f3fc4551abe0d6a34963381fff41159c3
Ian Tjok <ianptjok@hotmail.com>
2025-03-14 13:38:53 -0700

    Removed debugging console lines from authMiddleware.js
    Removed debugging console lines from authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,4 @@
 const isAuthenticated = (req, res, next) => {
-    console.log(req.user)
-    console.log("Session ID:", req.sessionID); // Debugging
-    console.log("User:", req.user); // Debugging
     if (req.isAuthenticated()) {
         return next(); // User is authenticated, proceed to the next middleware/route
     }
